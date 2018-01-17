fe68d4ffba7119327af3732fd04b76cc2f8e387f
Sebastian Wagner <wagner@cert.at>
2018-01-17 17:50:39 +0100

    Add database querying

--- generate-js.py
+++ generate-js.py
@@ -7,24 +7,30 @@ Created on Tue Jan 16 17:45:45 2018
 """
 import argparse
 import configparser
+import sys
+
+import psycopg2
+import psycopg2.extras
 
 
 VERSION = '0.1'
 
 TRACE = """
-var {section} = {
+var {section} = {{
   x: {x},
   y: {y},
-  name: '{name}',
+  name: '{section}',
   type: 'bar',
-};
+}};
 """
 
 
-def read_config(filename):
-    config = configparser.ConfigParser()
-    config.read(filename)
-    return config
+class ConnectionCache(object):
+    def __getitem__(self, key):
+        if key not in self.__dict__:
+            self.__dict__[key] = psycopg2.connect(dsn=key).cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        return self.__dict__[key]
+CONNECTIONS = ConnectionCache()
 
 
 def main():
@@ -37,10 +43,41 @@ def main():
                         action='version', version=VERSION)
     parser.add_argument('config', help='configuration file to use')
     parser.add_argument('-o', '--output',
-                        help='output file')
+                        help='output file, default: stdout',
+                        default=sys.stdout)
+    parser.add_argument('--dsn',
+                        help='DSN connection string',
+                        default=None)
 
     args = parser.parse_args()
-    config = read_config(args.config)
+    config = configparser.ConfigParser()
+    config.read(args.config)
+    traces = {}
+    for trace_name, section in config.items():
+        trace = {
+            'title': section.get('title', ''),
+            'width': section.get('width', ''),
+            'height': section.get('height', ''),
+            'dsn': args.dsn if args.dsn else section['dsn'],
+            }
+        if trace_name == 'DEFAULT':
+            continue
+        db = CONNECTIONS[trace['dsn']]
+        print(trace['dsn'])
+        print('Starting query for %s: %r' % (trace_name, section['query']))
+        result = db.execute(section['query'])
+        traces[trace_name] = trace
+        if result:
+            print(result)
+        else:
+            trace['x'] = trace['y'] = []
+
+    print('Rendering...')
+    with open('template.html') as template_handle:
+        TEMPLATE = template_handle.read()
+    print(TEMPLATE.format(width=trace['width'], height=trace['height'],
+                          traces=TRACE.format(x=trace['x'], y=trace['y'], section='foo'),
+                          traces_list=trace_name, title=trace['title']))
 
 
 if __name__ == '__main__':
