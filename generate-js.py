753d01e015b33bcadd93efc0444027e0db6338b4
Sebastian Wagner <wagner@cert.at>
2018-01-18 16:02:50 +0100

    Support multiple name columns

--- generate-js.py
+++ generate-js.py
@@ -6,31 +6,31 @@ Created on Tue Jan 16 17:45:45 2018
 @author: sebastian
 """
 import argparse
+import collections
 import configparser
 import datetime
 import sys
 
 import psycopg2
-import psycopg2.extras
 
 
 VERSION = '0.1'
 
 TRACE = """
-var {name} = {{
+var trace_{section_name}_{trace_name} = {{
   x: {x},
   y: {y},
-  name: '{name}',
+  name: '{trace_title}',
   type: 'bar',
 }};
 """
-GRAPH = """<div id="{name}" style="width:{width};height:{height};"></div>"""
+GRAPH = """<div id="{section_name}" style="width:{width};height:{height};"></div>"""
 PLOT = """
 {traces}
 
-var data_{name} = [{traces_list}];
+var data_{section_name} = [{traces_list}];
 
-var layout_{name} = {{
+var layout_{section_name} = {{
     barmode: '{barmode}',
     legend: {{
     x: 0,
@@ -43,7 +43,7 @@ var layout_{name} = {{
   height: {height},
   }};
 
-Plotly.newPlot('{name}', data_{name}, layout_{name});
+Plotly.newPlot('{section_name}', data_{section_name}, layout_{section_name});
 """
 TEMPLATE = """
 <html>
@@ -63,7 +63,7 @@ TEMPLATE = """
 class ConnectionCache(object):
     def __getitem__(self, key):
         if key not in self.__dict__:
-            self.__dict__[key] = psycopg2.connect(dsn=key).cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+            self.__dict__[key] = psycopg2.connect(dsn=key).cursor()
         return self.__dict__[key]
 CONNECTIONS = ConnectionCache()
 
@@ -89,44 +89,44 @@ def main():
     config.read(args.config)
     graphs = []
     plots = []
-    for trace_name, section in config.items():
-        traces = {}
-        if trace_name == 'DEFAULT':
+    for section_name, section in config.items():
+        if section_name == 'DEFAULT':
             continue
-        trace = {
-            'title': section.get('title', ''),
-            'width': section.get('width', ''),
-            'height': section.get('height', ''),
-            'dsn': args.dsn if args.dsn else section['dsn'],
-            'x': [],
-            'y': [],
-            }
-        db = CONNECTIONS[trace['dsn']]
-        print('Starting query for %s: %r' % (trace_name, section['query']), file=sys.stderr)
+        title = section.get('title', '')
+        width = section.get('width', '')
+        height = section.get('height', '')
+        dsn = args.dsn if args.dsn else section['dsn']
+        db = CONNECTIONS[dsn]
+        print('Starting query for %s: %r' % (section_name, section['query']), file=sys.stderr)
         db.execute(section['query'])
+        data = collections.defaultdict(lambda: ([], []))
         for row in db:
-            for rowkey, rowvalue in row.items():
-                if rowkey == 'count':
-                    trace['y'].append(rowvalue)
-                else:
-                    if isinstance(rowvalue, datetime.datetime):
-                        rowvalue = rowvalue.isoformat()
-                    trace['x'].append(rowvalue)
-        traces[trace_name] = trace
+            row_x = row[0]
+            row_y = row[-1]
+            row_names = tuple(row[1:-1])
+            if isinstance(row_x, datetime.datetime):
+                row_x = row_x.isoformat()
+            data[row_names][0].append(row_x)
+            data[row_names][1].append(row_y)
+        traces = []
+        for trace_names, trace_data in data.items():
+            traces.append(TRACE.format(section_name=section_name, trace_name='_'.join(trace_names),
+                                       trace_title=' '.join(trace_names),
+                                       x=trace_data[0], y=trace_data[1]))
 
-        graphs.append(GRAPH.format(name=trace_name, width=trace['width'], height=trace['height']))
-        plots.append(PLOT.format(name=trace_name,
-                                 width=trace['width'], height=trace['height'],
-                                 title=trace['title'],
-                                 traces_list=trace_name,
-                                 traces=TRACE.format(name=trace_name, x=trace['x'], y=trace['y']),
+        graphs.append(GRAPH.format(section_name=section_name, width=width, height=height))
+        plots.append(PLOT.format(section_name=section_name,
+                                 width=width, height=height,
+                                 title=title,
+                                 traces_list=', '.join(['trace_%s_%s' % (section_name, '_'.join(trace_name)) for trace_name in data.keys()]),
+                                 traces='\n'.join(traces),
                                  barmode=section.get('barmode', 'group')
                                  ))
 
     print('Rendering...', file=sys.stderr)
     plot = TEMPLATE.format(graphs='\n'.join(graphs),
                            plots='\n'.join(plots),
-                           title=trace['title'])
+                           )
     if not args.output:
         print(plot)
     else:
